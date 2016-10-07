36ebd2f5cfa9318dafdbfce02b0225b1cfa169d5
StevenLocke <slocke@mesosphere.io>
2016-10-07 16:30:14 -0700

    Updated server to use pipes for communication.

--- server.cpp
+++ server.cpp
@@ -7,6 +7,7 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/select.h>
 
 #include "common.h"
 
@@ -16,6 +17,13 @@ void usage(char *cmd)
   exit(1);
 }
 
+int max3(int a, int b, int c)
+{
+  int m = a;
+  (m < b) && (m = b);
+  (m < c) && (m = c);
+  return m;
+}
 
 int main(int argc, char *argv[])
 {
@@ -76,6 +84,31 @@ int main(int argc, char *argv[])
       error("ERROR on accept");
     }
 
+    int stdin_pipe[2];
+    if (pipe(stdin_pipe) < 0) {
+      error("ERROR creating a std_in pipe.");
+    }
+
+    int stdout_pipe[2];
+    if (pipe(stdout_pipe) < 0) {
+      error("ERROR creating a std_out pipe.");
+    }
+    if (make_non_blocking(stdout_pipe[0]) < 0) {
+      error("ERROR making stdout_pipe non blocking");
+    }
+
+    int stderr_pipe[2];
+    if (pipe(stderr_pipe) < 0) {
+      error("ERROR creating a std_err pipe.");
+    }
+    if (make_non_blocking(stderr_pipe[0]) < 0) {
+      error("ERROR making stderr_pipe non blocking");
+    }
+
+    if (make_non_blocking(newsockfd) < 0) {
+      error("ERROR making sockfd non blocking");
+    }
+
     int pid = fork();
 
     if (pid == 0) {
@@ -87,14 +120,21 @@ int main(int argc, char *argv[])
 
       char **cmd = build_cmd_array(message);
 
-      if (dup2(newsockfd, STDIN_FILENO) != 0 ||
-          dup2(newsockfd, STDOUT_FILENO) != 1 ||
-          dup2(newsockfd, STDERR_FILENO) != 2 ) {
+      if (dup2(stdin_pipe[0], STDIN_FILENO) != 0 ||
+          dup2(stdout_pipe[1], STDOUT_FILENO) != 1 ||
+          dup2(stderr_pipe[1], STDERR_FILENO) != 2 ) {
         error("ERROR duplicating socket for stdin/stdout/stderr");
       }
 
       close(newsockfd);
       close(sockfd);
+
+      close(stdin_pipe[0]);
+      close(stdout_pipe[0]);
+      close(stderr_pipe[0]);
+      close(stdin_pipe[1]);
+      close(stdout_pipe[1]);
+      close(stderr_pipe[1]);
 
       result = execvp(cmd[0], cmd);
       if (result < 0) {
@@ -102,13 +142,66 @@ int main(int argc, char *argv[])
       }
     }
 
-    close(newsockfd);
+    while(true) {
+      fd_set readfds;
+      FD_ZERO(&readfds);
+ 
+      FD_SET(stdout_pipe[0], &readfds);
+      FD_SET(stderr_pipe[0], &readfds);
 
-    int status;
-    result = wait_all(pid, &status);
-    if (result < 0) {
-      error("ERROR waiting for pid");
+      result = select(
+        max3(
+          sockfd, 
+          stdout_pipe[0], 
+          stderr_pipe[0]) + 1, 
+        &readfds, 
+        NULL,
+        NULL,
+        NULL);
+
+      if (result < 0) {
+        error("ERROR waiting on select");
+      }
+
+      if (result == 0) {
+        continue;
+      }
+
+      int infd, outfd;
+
+      if (FD_ISSET(stdout_pipe[0], &readfds)) {
+        infd = stdout_pipe[0];
+        outfd = newsockfd;
+      }
+      if (FD_ISSET(stderr_pipe[0], &readfds)) {
+        infd = stderr_pipe[0];
+        outfd = newsockfd;
+      }
+
+      char buffer[256];
+
+      int n = read_all(infd, buffer, 256);
+      if (n < 0) {
+        error("ERROR reading from infd");
+      }
+
+      n = write_all(outfd, buffer, n);
+      if (n < 0) {
+        error("ERROR writing to outfd");
+      }
+
+      if (n < sizeof(buffer)/sizeof(buffer[0])) {
+        break;
+      }
     }
+
+    close(newsockfd);
+    close(stdin_pipe[0]);
+    close(stdout_pipe[0]);
+    close(stderr_pipe[0]);
+    close(stdin_pipe[1]);
+    close(stdout_pipe[1]);
+    close(stderr_pipe[1]);  
   }
 
   close(sockfd);
