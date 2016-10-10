3efd943fa51ef0c57d2f369405fccdbc08287d75
Kevin Klues <klueska@gmail.com>
2016-10-10 16:52:13 -0700

    Added helpers for sending termios and winsize messages.

--- msgs.h
+++ msgs.h
@@ -139,6 +139,46 @@ static inline int send_io_msg(int fd, int destfd, char *buffer, int size)
 }
 
 
+static inline int send_termios_msg(int fd, struct termios *termios)
+{
+  struct msg_wrapper message;
+  message.type = TERMIOS_MSG;
+  message.msg.termios.termios = *termios;
+
+  int n = write_all(fd, (char*)&message, sizeof(message.type));
+  if (n < 0) {
+    return n;
+  }
+
+  n = write_all(fd, (char*)&message.msg.termios, sizeof(struct termios_msg));
+  if (n < 0) {
+    return n;
+  }
+
+  return 0;
+}
+
+
+static inline int send_winsize_msg(int fd, struct winsize *winsize)
+{
+  struct msg_wrapper message;
+  message.type = WINSIZE_MSG;
+  message.msg.winsize.winsize = *winsize;
+
+  int n = write_all(fd, (char*)&message, sizeof(message.type));
+  if (n < 0) {
+    return n;
+  }
+
+  n = write_all(fd, (char*)&message.msg.winsize, sizeof(struct winsize_msg));
+  if (n < 0) {
+    return n;
+  }
+
+  return 0;
+}
+
+
 static inline int recv_cmd_msg(int fd, struct msg_wrapper **message)
 {
   struct cmd_msg cmd_msg;
